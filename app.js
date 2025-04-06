9261821f08c6f5f493943ca5c2e82a1b77556ecc
Brenda De La Cruz <delabrenda00@gmail.com>
2025-04-06 11:58:35 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const readline = require("readline-sync")
+
+function add (num1, num2) {
+    return num1 + num2
+}
+
+console.log(add(2, 3));
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(4, 5));
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(divide(6, 3));
+
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subtract(4,2));
+
